c172d3c89e43779256b25e52fd20313c1feb82d8
PavelPoz <79132207654@yandex.ru>
2022-11-12 14:57:58 +0700

    Задача про вывод числа и минуса

--- Semi001/Program.cs
+++ Semi001/Program.cs
@@ -26,6 +26,7 @@ else
 }
 */
 // Задача про дни недели, ввод в консоль  вывод
+/*
 Console.WriteLine("Введите число дня недели: ");
 string? input = Console.ReadLine();
 int a = Convert.ToInt32(input);
@@ -57,3 +58,18 @@ if (a == 7)
 {
    Console.WriteLine("Воскресенье");
 }
+*/
+/* Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"
+*/
+/*
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = -number;
+while (number1 < number)
+{
+    Console.Write(number1);
+    number1 = ( number1 + 1);
+}
+*/
