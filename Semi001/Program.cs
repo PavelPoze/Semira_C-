ef06db9f203c17e54261a84ab9793538f28ce6ed
PavelPoz <79132207654@yandex.ru>
2022-11-12 12:54:01 +0700

    Задача 1

--- Semi001/Program.cs
+++ Semi001/Program.cs
@@ -1,8 +1,19 @@
-﻿//задача с вводом числа и вопросом к пользователю
-Console.Write("Введите число: ");
-string? input = Console.ReadLine();
-int number = Convert.ToInt32(input);
+﻿/*Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25, b = 5 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> да 
+a = -3 b = 9 -> нет
+*/
 
-int sgr = number * number;
+int a = 25;
 
-Console.WriteLine(number + " ->" + sgr);
+int b = 5;
+
+if ( a == b * b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
